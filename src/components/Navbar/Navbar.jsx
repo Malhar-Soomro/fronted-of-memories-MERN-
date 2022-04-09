b7233601a8798e5e1b52ca94350e10a7ea4a2383
Malhar-Soomro <malharsoomro2@gmail.com>
2022-04-09 18:25:25 +0500

    google auth implemented, and works fine

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core'
 import useStyles from "./styles";
 import memories from "../../Images/memories.png";
-import { Link } from "react-router-dom";
-
+import { useDispatch } from 'react-redux';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { LOGOUT } from "../../constants/actionTypes"
 
 const Navbar = () => {
+
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
     const classes = useStyles();
-    const user = null;
+
+    const logout = () => {
+        dispatch({ type: LOGOUT })
+        navigate("/auth")
+        setUser(null)
+    }
+
+    useEffect(() => {
+        // const token = user?.token;
+        setUser(JSON.parse(localStorage.getItem("profile")));
+    }, [location]);
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className>
@@ -19,18 +36,19 @@ const Navbar = () => {
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}
-                        >{user.result.name.charAt(0)}</Avatar>
+                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}
+                        >{user?.result.name.charAt(0)}</Avatar>
                         <Typography
                             className={classes.userName}
                             variant="h6">
-                            {user.result.name}
+                            {user?.result.name}
                         </Typography>
                         <Button
                             variant="contained"
                             className={classes.logout}
+                            onClick={logout}
                             color="secondary"
-                        >
+                        >Logout
                         </Button>
                     </div>
                 ) : (
