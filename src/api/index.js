2fcb9805d75335e9e5876ebf526ba040e96fbdaf
Malhar-Soomro <malharsoomro2@gmail.com>
2022-04-11 15:25:12 +0500

    server url changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://backend-of-memories.herokuapp.com" });
 
 //for the middleware, which was setup on backend
 API.interceptors.request.use((req) => {
@@ -10,6 +11,7 @@ API.interceptors.request.use((req) => {
     return req;
 });
 
+// https://backend-of-memories.herokuapp.com/
 // const url = "https://backend-of-memories.herokuapp.com/posts";
 
 
